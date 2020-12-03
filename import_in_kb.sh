cb0648f5fc8ee0a607288ef2ee960e36868f0888
gnebbia <16106651+gnebbia@users.noreply.github.com>
2020-12-03 03:04:59 +0100

    Periodic Update

--- import_in_kb.sh
+++ import_in_kb.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/env  sh
+# Script used to import all the directory structure into kb
+
 kb add ./cheatsheet/*                   --category "cheatsheet"
 kb add ./config/*                       --category "config"
 kb add ./list/*                         --category "list"
-kb add ./programming_languages/bash/*   --category "programming"
-kb add ./programming_languages/python/* --category "programming"
+kb add ./programming/bash/*             --category "programming/bash"
+kb add ./programming/python/*           --category "programming/python"
 kb add ./pt/*                           --category "procedure"
 kb add ./theory/*                       --category "theory"
 kb add ./links/*                        --category "link"
+kb add ./lang/russian/*                 --category "lang/russian"
